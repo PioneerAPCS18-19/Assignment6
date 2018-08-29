c95477a572da817b26a8cd20e0d41c69d412344c
codytheking <codytheking93@gmail.com>
2018-08-29 13:32:35 -0700

    starter code

---
+++ CurrencyConversion.java
@@ -0,0 +1,61 @@
+
+/**
+ * This class has methods for converting currency.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+import java.util.Scanner;
+
+public class CurrencyConversion
+{
+    public static void main(String[] args)
+    {
+        // call printWelcome method (1 line)
+        
+        
+        // take input for US Dollars (2 lines)
+        
+        
+        
+        // convert usd to cad by calling method and 
+        // storing value in a variable (1 line)
+        
+        
+        // call printResults method (1 line)
+        
+    }
+    
+    /**
+     * This method does not take any parameters and does not
+     * return anything. It prints what program does and gives
+     * a prompt for input (but doesn't take input).
+     */
+    public static void printWelcome()
+    {
+        // your implementation...
+    }
+    
+    /**
+     * 1 USD = 1.25277 CAD
+     * 
+     * This method takes a double as a parameter and converts
+     * that value using the above conversion rate. It returns
+     * the conversion as a double.
+     */
+    
+    // write method heading here...
+    {
+        // your implementation...
+    }
+    
+    /**
+     * This method takes two doubles as parameters and prints
+     * the results of the conversions. It does not return anything.
+     */
+    public static void printResults(double usd, double cad)
+    {
+        // your implementation...
+    }
+}
